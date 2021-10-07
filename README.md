59b0360b80603f95c1d51acb5c0cffbba6143845
Tung Nguyen <tung.nguyen@geocomply.com>
2021-10-07 17:49:31 +0700

    commit

--- README.md
+++ README.md
@@ -15,17 +15,3 @@ $ gcc -shared -o vincenty.so -fPIC vincenty.c
 $ gcc -shared -o georef.so -fPIC georef.c
 $ python3 main.py
 ```
-
-### Result
-
-Time:
-
-```
-                Function    Distance (m)     Time (s)
-            great_circle   864214.494339     7.813234
-                geodesic   866455.432910   161.672827
-     custom_great_circle   864214.494339     2.767540
-         custom_geodesic   866455.432912     9.695861
-    C++ Geo Great Circle   866455.432913     1.116327
-            C++ Geodesic   866455.432915     1.378029
-```
--- main.py
+++ main.py
@@ -1,91 +1,10 @@
-import argparse
 import time
 import pathlib
 from ctypes import CDLL, c_double
-from math import sin, cos, asin, sqrt, radians, tan, atan2, pi
 from geopy.distance import great_circle, geodesic
 
 
-N = 1000000
-
-
-class custom_great_circle(great_circle):
-    """Haversine formula"""
-
-    RADIUS = 6371.009
-
-    def measure(self, a, b):
-        lat1, lng1 = radians(a[0]), radians(a[1])
-        lat2, lng2 = radians(b[0]), radians(b[1])
-
-        delta_lat = lat2 - lat1
-        delta_long = lng2 - lng1
-
-        haversine = sin(delta_lat/2)**2 + cos(lat1) * \
-            cos(lat2)*sin(delta_long/2)**2
-        return asin(sqrt(haversine))*2*self.RADIUS
-
-
-class custom_geodesic(geodesic):
-    """Vincenty formula"""
-
-    ELLIPSOID = (6378.137, 6356.7523142, 1 / 298.257223563)
-
-    def measure(self, a, b):
-        lat1, lng1 = radians(a[0]), radians(a[1])
-        lat2, lng2 = radians(b[0]), radians(b[1])
-
-        a, b, f = self.ELLIPSOID
-        L = lng2 - lng1
-
-        u1 = atan2((1-f) * tan(lat1), 1)
-        u2 = atan2((1-f) * tan(lat2), 1)
-
-        sinU1, cosU1 = sin(u1), cos(u1)
-        sinU2, cosU2 = sin(u2), cos(u2)
-
-        sinU1sinU2 = sinU1 * sinU2
-        sinU1cosU2 = sinU1 * cosU2
-        cosU1cosU2 = cosU1 * cosU2
-        cosU1sinU2 = cosU1 * sinU2
-
-        lamda, lamdaP, iter_limit = L, 2*pi, 100
-        while iter_limit > 0 and abs(lamda-lamdaP) > 1e-12:
-            sinLambda, cosLambda = sin(lamda), cos(lamda)
-            sinSigma = sqrt((cosU2*sinLambda)**2 +
-                            (cosU1sinU2-sinU1cosU2*cosLambda)**2)
-
-            if sinSigma == 0:
-                return -1
-
-            cosSigma = sinU1sinU2 + cosU1cosU2*cosLambda
-            sigma = atan2(sinSigma, cosSigma)
-            sinAlpha = cosU1cosU2*sinLambda/sinSigma
-            cosSqAlpha = 1.0 - sinAlpha**2
-
-            if cosSqAlpha < 1e-15:
-                cos2SigmaM = 0
-            else:
-                cos2SigmaM = cosSigma - 2.0 * sinU1sinU2/cosSqAlpha
-
-            C = f/16.0 * cosSqAlpha * (4.0 + f*(4.0 - 3.0*cosSqAlpha))
-            lamdaP = lamda
-            lamda = L + (1.0-C)*f*sinAlpha*(
-                sigma + C*sinSigma * (cos2SigmaM + C*cosSigma*(-1.0 + 2.0*cos2SigmaM**2)))
-            iter_limit -= 1
-
-        if iter_limit == 0:
-            return -1
-
-        uSq = cosSqAlpha * ((a/b)**2 - 1.0)
-        A = 1.0 + uSq / 16384.0 * \
-            (4096.0 + uSq * (-768.0 + uSq * (320.0 - 175.0 * uSq)))
-        B = uSq / 1024.0 * (256.0 + uSq * (-128.0 + uSq * (74.0 - 47.0 * uSq)))
-        deltaSigma = B * sinSigma * (
-            cos2SigmaM + B / 4.0 * (
-                cosSigma * (-1.0 + 2.0 * cos2SigmaM * cos2SigmaM) -
-                B / 6.0 * cos2SigmaM * (-3.0 + 4.0 * sinSigma * sinSigma) * (-3.0 + 4.0 * cos2SigmaM * cos2SigmaM)))
-        return b * A * (sigma - deltaSigma)
+N = 100000
 
 
 def test_dist_func(func):
@@ -103,7 +22,7 @@ def test_dist_func(func):
 
 def load_cdll(filename, funcname):
     cfile = CDLL('{}/{}.so'.format(pathlib.Path().absolute(), filename))
-    func = cfile.__getattr__(funcname)
+    func = getattr(cfile, funcname)
     func.argtypes = [c_double, c_double, c_double, c_double]
     func.restype = c_double
     return func
@@ -119,46 +38,9 @@ def test_time_consuming():
     print('%30s %13s %10s' % ('Function', 'Distance (m)', 'Time (s)'))
     test_dist_func(great_circle)
     test_dist_func(geodesic)
-    test_dist_func(custom_great_circle)
-    test_dist_func(custom_geodesic)
     test_dist_func(georef_func)
     test_dist_func(vincenty_func)
 
 
-def test_accuracy():
-    with open('locations.txt', 'r') as f:
-        locations = [list(map(float, s.split())) for s in f.readlines()]
-
-    funcs = [great_circle, custom_great_circle,
-             georef_func, custom_geodesic, vincenty_func]
-    max_diff = [0.0, 0.0, 0.0, 0.0, 0.0]
-    p_max_diff = [0.0, 0.0, 0.0, 0.0, 0.0]
-    sum_diff = [0.0, 0.0, 0.0, 0.0, 0.0]
-    p_sum_diff = [0.0, 0.0, 0.0, 0.0, 0.0]
-
-    print(len(locations))
-    for loc in locations:
-        p1 = (loc[0], loc[1])
-        p2 = (loc[2], loc[3])
-        dist = geodesic(p1, p2).meters
-        for i, func in enumerate(funcs):
-            if func.__name__ in ('C++ Geodesic', 'C++ Geo Great Circle'):
-                d = func(*loc)
-            else:
-                d = func(p1, p2).meters
-            diff = abs(dist-d)
-            max_diff[i] = max(max_diff[i], diff)
-            sum_diff[i] += diff
-            p_max_diff[i] = max(p_max_diff[i], diff/dist*100.0)
-            p_sum_diff[i] += diff/dist*100.0
-    print(max_diff)
-    print(*map(lambda s: s/len(locations), sum_diff))
-    print(p_max_diff)
-    print(*map(lambda s: s/len(locations), p_sum_diff))
-
-    # GC , custom GC, C++ GC, custom Geo, C++ Geo
-
-
 if __name__ == '__main__':
     test_time_consuming()
-    # test_accuracy()
